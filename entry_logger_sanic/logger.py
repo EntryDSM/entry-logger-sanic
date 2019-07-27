c9771bd5e1e6a00867edf0b2bcf1e8082d5ef2bd
Seonghyeon Kim <artoria@artoria.us>
2019-07-27 13:24:09 +0900

    ADD: slack integration

--- entry_logger_sanic/logger.py
+++ entry_logger_sanic/logger.py
@@ -1,6 +1,7 @@
 import json
 import logging
 import os
+import requests
 import traceback
 from datetime import datetime
 
@@ -11,6 +12,11 @@ SERVICE_NAME = str()
 
 
 class JSONLogFormatter(logging.Formatter):
+    def __init__(self, webhook, maintainer):
+        self.webhook = webhook
+        self.maintainer = maintainer
+        super().__init__()
+
     def format(self, record):
         log = {
             "level": record.levelname,
@@ -25,6 +31,30 @@ class JSONLogFormatter(logging.Formatter):
             if record.exc_info
             else "",
         }
+        if self.webhook and record.exc_info:
+            requests.post(
+                self.webhook,
+                json={
+                    "attachments": [
+                        {
+                            "fallback": "an error occurred",
+                            "color": "#FB0000",
+                            "author_name": SERVICE_NAME,
+                            "title": record.getMessage(),
+                            "text": f"단 하나의 진실을 꿰뚫는 겉 보기엔 어린이, 두뇌는 어른! 그 이름은 명탐정 코난!\n<@{self.maintainer}>! *범인은 바로 당신이야!*\nException in detail:\n```{''.join(traceback.format_exception(*record.exc_info))}```",
+                            "mrkdwn_in": ["text"],
+                            "fields": [
+                                {
+                                    "title": "Priority",
+                                    "value": "High",
+                                    "short": False
+                                }
+                            ],
+                            "ts": int(datetime.now().timestamp())
+                        }
+                    ]
+                }
+            )
         return json.dumps(log)
 
 
@@ -64,7 +94,13 @@ def set_logger(app: Sanic, log_path: str):
         f"Service log will saved at {os.path.abspath(f'{log_path}/{SERVICE_NAME}.log')}"
     )
 
-    _set_sanic_logger(log_path)
+    webhook = app.config.get("SLACK_WEBHOOK_URL")
+    maintainer = app.config.get("SLACK_MAINTAINER_ID")
+
+    if not webhook:
+        sanic_logger.info("Slack integration was not set")
+
+    _set_sanic_logger(log_path, webhook, maintainer)
     logger = _set_request_logger(log_path)
 
     @app.middleware("request")
@@ -96,8 +132,8 @@ def set_logger(app: Sanic, log_path: str):
         logger.info(json.dumps(log))
 
 
-def _set_sanic_logger(log_path):
-    handler = _create_handler(log_path, JSONLogFormatter())
+def _set_sanic_logger(log_path, webhook, maintainer):
+    handler = _create_handler(log_path, JSONLogFormatter(webhook, maintainer))
 
     sanic_logger.addHandler(handler)
 
